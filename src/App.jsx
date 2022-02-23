5d3320f2368cdf73b8a088b863ac04f4f553cdd1
Takahisa Hirano <54578410+Hiratto@users.noreply.github.com>
2022-02-23 21:23:05 +0900

    JSXの中にstyleを書く ＞style={}の中に書く ＞styleは文字列として認識させないと×、''で囲うこと ＞外側の{}はJS、内側はオブジェクトとして ＞CSSではfont-sizeだが、JSではキャメルケースで書くこと ＞関数を使う際はそのまま{}に入れる

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "red",
+    // CSSではfont-sizeだが、JSではキャメルケースで書くこと
+    fontSize: "18px"
+  };
   return (
     <>
-      <h1>Hello!!</h1>
-      <p>World!!</p>
+      {/* 外側の{}はJS、内側はオブジェクトとして */}
+      {/* styleは文字列として認識させないと×、''で囲うこと */}
+      <h1 style={{ color: "red" }}>Hello!!</h1>
+      {/* 関数を使う際はそのまま{}に入れる */}
+      <p style={contentStyle}>World!!</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
