b50fb26d26b477149a1a14e0b5e374cf386999cc
Takahisa Hirano <54578410+Hiratto@users.noreply.github.com>
2022-02-23 21:58:47 +0900

    porpsを分割代入に書き換え

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
   const contentLadyStyle = {
-    color: props.color,
+    color: color,
     fontSize: "18px"
   };
-  return <p style={contentLadyStyle}>{props.children}</p>;
+  return <p style={contentLadyStyle}>{children}</p>;
 };
 
 export default ColorfulMessage;
